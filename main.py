5ce5f497a9d920593716280d9fde83096b748552
Dmitriy <patrickjsmirnov@gmail.com>
2018-12-30 18:25:40 +0300

    request vk

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 
-from requests_html import HTMLSession
-session = HTMLSession()
+import requests
 
-r = session.get('https://apptica.com/')
+group_id = '-22746750'
+url = 'https://api.vk.com/method/wall.get'
+token = 'generate_your_token'
+
+r = requests.get(url, params={
+    'owner_id': group_id,
+    'count': 100,
+    'offset': 0,
+    'access_token': token,
+    'v': 5.52
+})
+
+print(r.json())
