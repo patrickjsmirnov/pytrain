ee47f4d8c01e4cbc3d8333721ea9bc8a784300ac
Dmitriy <patrickjsmirnov@gmail.com>
2019-01-02 22:07:26 +0300

    posgres, psycorg2, db, first db-request

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-
 import requests
+import json
+import psycopg2
 
-group_id = '-22746750'
-url = 'https://api.vk.com/method/wall.get'
-token = 'generate_your_token'
 
-r = requests.get(url, params={
-    'owner_id': group_id,
-    'count': 100,
-    'offset': 0,
-    'access_token': token,
-    'v': 5.52
-})
+def main():
+    group_id = '-22746750'
+    url = 'https://api.vk.com/method/wall.get'
+    token = 'generate_your_token'
 
-print(r.json())
+    r = requests.get(url, params={
+        'owner_id': group_id,
+        'count': 100,
+        'offset': 0,
+        'access_token': token,
+        'v': 5.52
+    })
+
+    items = r.json()['response']['items']
+    # print(json.dumps(items, indent=4, sort_keys=True))
+
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='123', host='localhost')
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO test VALUES {}".format("(4, 'test4')")
+    select_query = 'SELECT * FROM test LIMIT 10'
+
+    cursor.execute(insert_query)
+
+    cursor.execute(select_query)
+    records = cursor.fetchall()
+
+    for row in records:
+        print(row[1])
+
+
+main()
